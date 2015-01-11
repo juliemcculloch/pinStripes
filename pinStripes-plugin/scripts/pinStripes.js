d9015d2305e93725dcfd1367d63901cdb239f848
juliemcculloch <julie.mcculloch@hotmail.com>
2015-01-11 08:34:40 -0500

    updated code

--- pinStripes-plugin/scripts/pinStripes.js
+++ pinStripes-plugin/scripts/pinStripes.js
@@ -2,7 +2,7 @@ $.fn.pinStripes = function(options) {
 	var settings = $.extend({
 
 	 spacing: 15,  // change the line spacing here
-	 color: '#FFFFFF',  //  change hex colour here
+	 color: '#FFFFFF',  //  change colour here - use hex colour only
 	 opacity: 0.5  //  change the opacity here
 
 	}, options)
@@ -17,9 +17,7 @@ $.fn.pinStripes = function(options) {
 		 $('.stripe').eq(i).css( "left", i*settings.spacing+'px' );
 		 $('.stripe').eq(i).css( "border-color", settings.color );
 		 $('.stripe').eq(i).css( "opacity", settings.opacity ); 
-
- 		// Bonus code by Sacha Sayan - undo and see what happens 
-		 	// $('.stripe').eq(i).css( "transform", "rotate(" +Math.random()*360+"deg)");
+		 
 
 		 // Assign the animation - the speed it scrolls on the page
 		 var randomNumber = Math.random()*12000+100; 
