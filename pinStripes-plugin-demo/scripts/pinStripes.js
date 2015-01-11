9468be503872214a56ed66d15e1924739381d4fa
juliemcculloch <julie.mcculloch@hotmail.com>
2015-01-11 07:59:19 -0500

    made changes to code

--- pinStripes-plugin-demo/scripts/pinStripes.js
+++ pinStripes-plugin-demo/scripts/pinStripes.js
@@ -1,9 +1,11 @@
 $.fn.pinStripes = function(options) {
 	var settings = $.extend({
+		
 
 	 spacing: 15,  // change the line spacing here
-	 color: '#FFFFFF',  //  change colour here
+	 color: '#FFFFFF',  //  change colour here - use hex colour only
 	 opacity: 0.5  //  change the opacity here
+	 
 
 	}, options)
 
@@ -18,7 +20,7 @@ $.fn.pinStripes = function(options) {
 		 $('.stripe').eq(i).css( "border-color", settings.color );
 		 $('.stripe').eq(i).css( "opacity", settings.opacity ); 
 
- 		// Bonus code by Sacha Sayan - undo and see what happens 
+ 		// Bonus code - undo and see what happens: 
 		 	// $('.stripe').eq(i).css( "transform", "rotate(" +Math.random()*360+"deg)");
 
 		 // Assign the animation - the speed it scrolls on the page
