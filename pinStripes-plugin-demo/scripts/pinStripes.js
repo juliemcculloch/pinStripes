d6afdf42d510c04459925ee7b0cc4744df37345f
juliemcculloch <julie.mcculloch@hotmail.com>
2015-01-11 08:38:06 -0500

    update code for plugin

--- pinStripes-plugin-demo/scripts/pinStripes.js
+++ pinStripes-plugin-demo/scripts/pinStripes.js
@@ -20,8 +20,6 @@ $.fn.pinStripes = function(options) {
 		 $('.stripe').eq(i).css( "border-color", settings.color );
 		 $('.stripe').eq(i).css( "opacity", settings.opacity ); 
 
- 		// Bonus code - undo and see what happens: 
-		 	// $('.stripe').eq(i).css( "transform", "rotate(" +Math.random()*360+"deg)");
 
 		 // Assign the animation - the speed it scrolls on the page
 		 var randomNumber = Math.random()*12000+100; 
